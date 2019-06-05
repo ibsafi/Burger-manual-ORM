1b9df08d9dda5ed624a9435b48c0a1992affd7a0
Ibrahim Safi <ibraheemsafi@gmail.com>
2019-06-05 15:11:41 -0400

    config

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config.json')[env];
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  host: config.host,
+  port: config.port,
+  user: config.username,
+  password: config.password,
+  database: config.database
 });
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
